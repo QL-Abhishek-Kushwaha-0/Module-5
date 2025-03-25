d998bcdf97a6a1dfe7b588af0f22266a1a5b7be7
Abhishek Kushwaha <abhishek.kushwaha@quokkalabs.com>
2025-03-25 11:28:47 +0530

    Module-6 : Task-1 -> Connecting Database and creating ORM for DB

--- Blog Application/Blog Application/Data/ApplicationDbContext.cs
+++ Blog Application/Blog Application/Data/ApplicationDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace Blog_Application.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Blog_Application.Models.Entities;
+
+namespace Blog_Application.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Posts)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);  // Change from Cascade to SetNull
+
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Author)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);  // Prevents cascade path conflicts
+        }
     }
 }
