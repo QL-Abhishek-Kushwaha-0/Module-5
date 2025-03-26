efc96c8943d8d7d3cdff870ecdd73550230fb375
Abhishek Kushwaha <abhishek.kushwaha@quokkalabs.com>
2025-03-26 11:40:07 +0530

    Module-6 : Task 2 (Partially Completed) -> Implemented JWT Token and Auth APIs

--- Blog Application/Blog Application/Controllers/AuthController.cs
+++ Blog Application/Blog Application/Controllers/AuthController.cs
@@ -1,36 +1,112 @@
-﻿using Blog_Application.Middlewares;
-using Blog_Application.Utils;
+﻿using Blog_Application.Data;
+using Blog_Application.DTO;
+using Blog_Application.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
+using Blog_Application.Middlewares;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Blog_Application.Controllers
 {
-
-
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private static Dictionary<int, string> Blogs = new()
-        {
-            { 1, "Understanding .NET Middleware" },
-            { 2, "Building REST APIs with ASP.NET Core" }
-        };
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
 
-        // Example  API for testing the Logging and Exception Handling   
-        [HttpGet("{id}")]
-        public IActionResult GetBlogById(int id)
+        public AuthController(ApplicationDbContext context, IConfiguration config)
         {
-            if (!Blogs.ContainsKey(id))
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == registerDto.Email);
+
+            if (existingUser != null)
             {
-                throw new GlobalException(); // This will be caught by ExceptionMiddleware
+                return Conflict(new { Response = new ApiResponse(false, 409, "User already exists!!!!") });
             }
-            
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+
+            var newUser = new User
+            {
+                Name = registerDto.Name,
+                Email = registerDto.Email,
+                Password = hashedPassword,
+                Username = registerDto.Username,
+                Role = registerDto.Role
+            };
+
+            _context.Users.Add(newUser);
+
+            await _context.SaveChangesAsync();
+
             return Ok(new
             {
-                response = new ApiResponse(true, 200, "Blog Fetched Successfully!!!"),
-                data = new { id, title = Blogs[id] }
+                response = new ApiResponse(true, 200, "User Registered Successfully!!!!"),
+                data = new { newUser.Name, newUser.Email, newUser.Username }
             });
+        }
 
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email);
+
+            if (existingUser == null)
+            {
+                return NotFound(new { response = new ApiResponse(false, 404, "No User Found!!!") });
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, existingUser.Password))
+            {
+                return Unauthorized(new { response = new ApiResponse(false, 401, "Unauthorized Access!!!") });
+            }
+
+            var token = GenerateJwtToken(existingUser.Name, existingUser.Email, existingUser.Role);
+            return Ok(new
+            {
+                response = new ApiResponse(true, 200, "Logged In Successfully....."),
+                JwtToken = token,
+                data = new { existingUser.Name, existingUser.Email, existingUser.Username }
+            });
+        }
+
+
+        // Method to Generate the JWT Token
+        private string GenerateJwtToken(string name, string email, UserRole role)
+        {
+            var jwtSettings = _config.GetSection("JwtSettings");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            string roleStr = Convert.ToString(role) ?? "";
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, name),
+                new(ClaimTypes.Email, email),
+                new(ClaimTypes.Role, roleStr)
+            };
+
+            var token = new JwtSecurityToken(
+                    issuer: jwtSettings["Issuer"],
+                    audience: jwtSettings["Audience"],
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(double.Parse(jwtSettings["ExpiryMinutes"])),
+                    signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- Blog Application/Blog Application/DTO/LoginDto.cs
+++ Blog Application/Blog Application/DTO/LoginDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Blog_Application.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Application.DTO
 {
     public class LoginDto
     {
+        [EmailAddress(ErrorMessage ="Enter a Valid Email Address!!!!")]
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
     }
 }
